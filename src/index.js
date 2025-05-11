2e5dc1120b2cff7c934381a51b17c62e08bf6aae
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-05-11 20:16:22 +0300

    create collection of contacts via HTTP requests

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { setupServer } from './server.js';
+import { initMongoDB } from './db/initMongoConnection.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
+
+bootstrap();
