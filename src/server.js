bcfa8bc23bbcefabdba95d7a382a3f3f17a97857
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-05-15 23:38:12 +0300

    fixed the status field

--- src/server.js
+++ src/server.js
@@ -24,8 +24,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
-      status: 'success',
-      code: 200,
+      status: 200,
       message: 'Successfully found contacts!',
       data: {
         contacts,
@@ -39,16 +38,13 @@ export const setupServer = () => {
 
     if (!contact) {
       res.status(404).json({
-        status: 'error',
-        code: 404,
         message: 'Contact not found',
       });
       return;
     }
 
     res.status(200).json({
-      status: 'success',
-      code: 200,
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
