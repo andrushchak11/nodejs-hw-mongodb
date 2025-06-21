3cea7aaa8d2054f65f83784f42fca695c38b52a7
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-06-21 14:04:56 +0300

    fix: ensure Swagger docs work and are exposed on /api-docs

--- src/server.js
+++ src/server.js
@@ -26,12 +26,12 @@ export const setupServer = () => {
     }),
   );
 
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router);
 
   app.use('*', notFoundHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 
   app.use(errorHandler);
 
